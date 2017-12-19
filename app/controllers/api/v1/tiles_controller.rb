892c482b75e255694f4342832bd847f5276df51e
Bryce Johnston <bryce@agdeveloper.com>
2017-12-19 12:02:42 -0600

    Add order by date and adjust date param checks on tile controller

--- app/controllers/api/v1/tiles_controller.rb
+++ app/controllers/api/v1/tiles_controller.rb
@@ -3,8 +3,13 @@ class API::V1::TilesController < API::APIController
   def index
     if params.has_key?(:mgrs)
       @tiles = Tile.where(mgrs_grid_name: params[:mgrs])
-      @tiles.where("date >= ?", params[:start_date]) if params.has_key?(:start_date)
-      @tiles.where("date <= ?", params[:end_date]) if params.has_key?(:end_date)
+      if params.has_key?(:start_date)
+        @tiles.where("date >= ?", params[:start_date])
+      end
+      if params.has_key?(:end_date)
+        @tiles.where("date <= ?", params[:end_date])
+      end
+      @tiles.order(date: :asc)
     else
       @tiles = []
     end
